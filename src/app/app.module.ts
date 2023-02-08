b53f36ed8f1fe285f43c29543a6b252fbf599f26
mrinal-kishore-1998 <mrinal1117.kishore@gmail.com>
2023-02-08 20:14:48 +0530

    Handling user input by Two-Way-Binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { PersonsComponent } from './persons/persons.component';
 import { PersonInputComponent } from './persons/person-input.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent, PersonsComponent, PersonInputComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/persons/person-input.component.ts
+++ src/app/persons/person-input.component.ts
@@ -6,10 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./person-input.component.css'],
 })
 export class PersonInputComponent {
+  enteredPersonName: string = '';
+
   /**
    * onCreatePerson(personName) is used to create new user
    */
-  public onCreatePerson(personName: string) {
-    console.log(personName);
+  public onCreatePerson() {
+    console.log('Created a new persons: ' + this.enteredPersonName);
+    this.enteredPersonName = '';
   }
 }
