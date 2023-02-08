edfa91371b658762402839edc35811a075391faf
mrinal-kishore-1998 <mrinal1117.kishore@gmail.com>
2023-02-08 19:21:04 +0530

    Manually creating & registering the component

---
+++ src/app/persons/persons.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-persons',
+  templateUrl: './persons.component.html',
+  //sstyleUrls: './persons.scomponent.css'
+})
+
+export class PersonsComponent {}
